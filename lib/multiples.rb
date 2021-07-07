badcf364d6850485d258161279605af33b8fcd0f
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-07-07 18:45:33 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    arr = (1...num).to_a
+    multiples = arr.select{|i| i % 3 == 0 || i % 5 == 0}
+    multiples
+end
+
+def sum_multiples(num)
+    collect_multiples(num).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        arr = (1...@limit).to_a
+        multiples = arr.select{|i| i % 3 == 0 || i % 5 == 0}
+        multiples
+    end
+    
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
